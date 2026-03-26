fc7b1b2c795fcbb44f3cc319c6b4dc935ceebdaf
ValB <valvatorezbraveheart@gmail.com>
2026-03-26 17:48:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/thoughtforfoods/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.thoughtforfoods;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * MainActivity handles the logic for the primary screen.
+ * It must extend AppCompatActivity to support older Android versions.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Links this Java class to the XML layout (activity_main.xml)
+        setContentView(R.layout.activity_main);
+    }
+}
